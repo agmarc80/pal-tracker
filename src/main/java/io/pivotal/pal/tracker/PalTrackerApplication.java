9ac32bbfacc11d6bdd6849c228a7c961ab347b45
agmarc80 <agmarc80@gmail.com>
2018-07-11 10:33:06 -0600

    codesync

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.mysql.cj.jdbc.MysqlDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
+import javax.sql.DataSource;
 import java.util.List;
 
 @SpringBootApplication
@@ -18,6 +20,14 @@ public class PalTrackerApplication {
     @Bean
     public TimeEntryRepository timeEntryRepository() {
         return new InMemoryTimeEntryRepository();
+
+    }
+
+    @Bean
+    public JdbcTimeEntryRepository jdbcTimeEntryRepository() {
+        MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+        return new JdbcTimeEntryRepository(dataSource);
 
     }
 
